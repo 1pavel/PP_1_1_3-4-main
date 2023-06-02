ca6b6a9b1fd997b205c37bfd7166b3c132586ba4
Pavel <pashmik@yandex.ru>
2023-06-02 20:43:01 +0300

    update Util and UserDaoHibernateImpl

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -3,12 +3,15 @@ package jm.task.core.jdbc.dao;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
-    private Session session = Util.getSessionFactory().openSession();
+    private SessionFactory sessionFactory = Util.getSessionFactory();
+    private Transaction transaction;
     public UserDaoHibernateImpl() {
 
     }
@@ -18,49 +21,77 @@ public class UserDaoHibernateImpl implements UserDao {
     public void createUsersTable() {
         String create = "CREATE TABLE IF NOT EXISTS users" +
                 "(id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(45) NOT NULL, lastName VARCHAR(45) NOT NULL , age INT(3));";
-        session.beginTransaction();
+
+        try (Session session = sessionFactory.openSession()) {
+        transaction = session.beginTransaction();
         Query query = session.createSQLQuery(create).addEntity(User.class);
         query.executeUpdate();
-        session.getTransaction().commit();
+        transaction.commit();
+        } catch(Exception e) {
+            transaction.rollback();
+        }
     }
 
     @Override
     public void dropUsersTable() {
         String drop = "drop table if exists users";
-        session.beginTransaction();
-        Query q = session.createSQLQuery(drop).addEntity(User.class);
-        q.executeUpdate();
-        session.getTransaction().commit();
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            Query q = session.createSQLQuery(drop).addEntity(User.class);
+            q.executeUpdate();
+            transaction.commit();
+    } catch(Exception e) {
+            transaction.rollback();
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        session.beginTransaction();
-        session.save(new User(name, lastName, age));
-        session.getTransaction().commit();
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            session.save(new User(name, lastName, age));
+            transaction.commit();
+        } catch(Exception e) {
+            transaction.rollback();
+        }
     }
 
     @Override
     public void removeUserById(long id) {
-        session.beginTransaction();
-        User user = session.find(User.class,id);
-        if(user != null) {
-        session.remove(user);
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            User user = session.find(User.class,id);
+            if(user != null) {
+                session.remove(user);
+            }
+            transaction.commit();
+        } catch (Exception e) {
+            transaction.rollback();
         }
-        session.getTransaction().commit();
     }
 
     @Override
     public List<User> getAllUsers() {
-        return session.createQuery("from User", User.class).list();
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            List<User> userList = session.createQuery("from User", User.class).list();
+            transaction.commit();
+            return userList;
+        } catch(Exception e) {
+            return null;
+        }
     }
 
     @Override
     public void cleanUsersTable() {
         String trun = "TRUNCATE TABLE users";
-        session.beginTransaction();
-        Query q = session.createSQLQuery(trun);
-        q.executeUpdate();
-        session.getTransaction().commit();
+        try (Session session = sessionFactory.openSession()) {
+         transaction = session.beginTransaction();
+         Query q = session.createSQLQuery(trun);
+         q.executeUpdate();
+         transaction.commit();
+    } catch (Exception e) {
+        transaction.rollback();
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -42,9 +42,9 @@ public class Util {
                     // Hibernate settings equivalent to hibernate.cfg.xml's properties
                     Properties settings = new Properties();
                     settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
-                    settings.put(Environment.URL, "jdbc:mysql://localhost:3306/mydbtest?useSSL=false");
-                    settings.put(Environment.USER, "root");
-                    settings.put(Environment.PASS, "root");
+                    settings.put(Environment.URL, URL); // "jdbc:mysql://localhost:3306/mydbtest?useSSL=false");
+                    settings.put(Environment.USER, USER);
+                    settings.put(Environment.PASS, PASSWORD);
                     settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
 
                     settings.put(Environment.SHOW_SQL, "true");
